ae28323fde244c60b36beedac830d310db61bed1
Konstantinos Chaitas <kostas@Konstantinoss-MacBook-Pro.local>
2018-11-21 14:45:03 +0100

    Return appropriate response if Lambda functions are invoked through the Lex-Bot

--- terraform/lambda_functions/Escalate_Incident.py
+++ terraform/lambda_functions/Escalate_Incident.py
@@ -12,26 +12,65 @@ def publish_to_connect_sns(payload):
     return response
 
 
+# -- AWS Lex Bot Intent response --
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
 def lambda_handler(event, context):
     
-    print(json.dumps(event))
+    event_response = json.dumps(event)
+
+    print(event_response)
+
+    if 'bot' in event_response:
+
+        parsed_event = json.loads(event_response)
+
+        escalation_target = parsed_event['currentIntent']['slotDetails']['escalationTarget']['originalValue']
+        message = parsed_event['currentIntent']['slotDetails']['message']['originalValue']
+
+        payload = {
+            'message': message,
+            'priority': 'high',
+            'escalation': escalation_target
+        }        
+
+        publish_to_connect_sns(payload)
+            
+        print(json.dumps(payload))
+
+        return close(
+            {}, 
+            'Fulfilled',
+            {
+                'contentType': 'PlainText',
+                'content': 'The incident with message ' + message + ' has been escalated to ' + escalation_target
+            }
+        )
+
+    else:
+        escalation_target = event_response['Details']['Parameters']['escalationTarget']
+        message = event_response['Details']['Parameters']['message']
     
-    escalation_target = event['Details']['Parameters']['escalationTarget']
-    message = event['Details']['Parameters']['message']
-    
-    print(json.dumps(escalation_target))
-    
-    payload = {
-        'message': message,
-        'priority': 'high',
-        'escalation': escalation_target
-    }        
-    
-    publish_to_connect_sns(payload)
-        
-    print(json.dumps(payload))
-    
-    
-    resultMap = {'escalation':'done'}
-    
-    return resultMap
+        payload = {
+            'message': message,
+            'priority': 'high',
+            'escalation': escalation_target
+        }        
+
+        publish_to_connect_sns(payload)
+            
+        print(json.dumps(payload))
+
+        resultMap = {'escalation':'done'}
+
+        return resultMap
--- terraform/lambda_functions/GetCurrentIncident_AWSConnect.py
+++ terraform/lambda_functions/GetCurrentIncident_AWSConnect.py
@@ -16,6 +16,20 @@ def get_key_from_ddb(key):
     return response
 
 
+# -- AWS Lex Bot Intent response --
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+
 def lambda_handler(event, context):
     counter = get_key_from_ddb('counter')
     current_key = counter['Item']['message']['S']
@@ -28,9 +42,22 @@ def lambda_handler(event, context):
     
     print(json.dumps(message))
 
-    response = {
-        'statusCode': 200,
-        'message':message
-    }
+    event_response = json.dumps(event)
 
-    return response
+    #Check if lambda is called from AWS Lex
+    if 'bot' in event_response:
+        return close(
+            {}, 
+            'Fulfilled',
+            {
+                'contentType': 'PlainText',
+                'content': message
+            }
+        )
+    else:
+        response = {
+            'statusCode': 200,
+            'message':message
+        }
+
+        return response
