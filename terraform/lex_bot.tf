82ec08b123263494fe6c52c7bb6ba9faf5afee0c
Konstantinos Chaitas <kostas@Konstantinoss-MacBook-Pro.local>
2018-11-22 19:57:58 +0100

    Fixing bug with automatic lex bot deployment

--- terraform/lex_bot.tf
+++ terraform/lex_bot.tf
@@ -1,6 +1,7 @@
 resource "null_resource" "test" {
+  depends_on = ["aws_lambda_function.GetCurrentIncident_AWSConnect", "aws_lambda_function.Escalate_Incident"]
   provisioner "local-exec" {
     command = "python deploy_bot.py"
-    working_dir = "../deployment"
+    working_dir = "../lex_bot"
   }
 }
