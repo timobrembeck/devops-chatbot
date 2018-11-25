352651e0e1e23c9d20518eb2d285a84d965bd4b5
Timo Ludwig <ti.ludwig@web.de>
2018-11-25 22:38:46 +0100

    format terraform files

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,26 +1,25 @@
-
-
 resource "aws_dynamodb_table" "alert_log" {
-   name = "alert-log"
-   hash_key = "messageID"
-   read_capacity = 20
-   write_capacity = 20
-   stream_enabled = true
-   stream_view_type = "NEW_AND_OLD_IMAGES"
+  name             = "alert-log"
+  hash_key         = "messageID"
+  read_capacity    = 20
+  write_capacity   = 20
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
 
-   attribute {
-      name = "messageID"
-      type = "S"
-   }
+  attribute {
+    name = "messageID"
+    type = "S"
+  }
 
-   tags {
-     Name = "DynamoDB alert log table"
-   }
+  tags {
+    Name = "DynamoDB alert log table"
+  }
 }
 
 resource "aws_dynamodb_table_item" "alert_log_counter_item" {
   table_name = "${aws_dynamodb_table.alert_log.name}"
   hash_key   = "${aws_dynamodb_table.alert_log.hash_key}"
+
   item = <<ITEM
 {
     "messageID": {"S": "counter"},
@@ -28,5 +27,4 @@ resource "aws_dynamodb_table_item" "alert_log_counter_item" {
     "active": {"B": "true"}
 }
 ITEM
-
 }
