58b9f4f8751423a00c08dd487051b578e2129e79
Aamir Suhial Mir <aamir.s.mir@campus.tu-berlin.de>
2019-01-13 10:01:54 +0100

    -> Added intent slots to Esclate incident intent

--- terraform/lex_bot.tf
+++ terraform/lex_bot.tf
@@ -1,8 +1,7 @@
 resource "null_resource" "test" {
   depends_on = [
     "aws_lambda_function.GetCurrentIncident_AWSConnect",
-    "aws_lambda_function.Escalate_Incident",
-    "aws_lambda_function.GetIncidentWithNumber"
+    "aws_lambda_function.Escalate_Incident"
   ]
 
   provisioner "local-exec" {
